711f556ce9ae87494ffaafe1f014d54781ca1c7b
master <alya26271@gmail.com>
2023-10-26 03:11:47 +0300

    Initial commit

---
+++ src/WordSplitterTest.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class WordSplitterTest {
+    public static void main(String[] args) {
+        WordSplitter wordSplitter = new WordSplitter();
+
+        String[] words = wordSplitter.split("Hello      world");
+
+        //[hello, world]
+        System.out.println(Arrays.toString(words));
+    }
+}
